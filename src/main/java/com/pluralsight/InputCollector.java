bb23ad7d844cf65722a018278969695c4f2d9ce3
Nachi4265 <hvirgen@my.yearupunited.org>
2025-10-09 17:49:51 -0400

    Started the skeleton of the menu (perfect for spooky season)

---
+++ src/main/java/com/pluralsight/InputCollector.java
@@ -0,0 +1,69 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class InputCollector {
+
+    //This is what will collect all our input
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String promptForString(String prompt){
+        System.out.print(prompt + ": ");
+        return scanner.nextLine().trim();
+    }
+
+    public static char promptForChar(String prompt){
+        System.out.println(prompt + ": ");
+        return scanner.next().charAt(0);
+    }
+
+    public static float promptForFloat(String prompt){
+
+        boolean isFloat = false;
+        float result = 0;
+
+        do{
+            try{
+                System.out.print(prompt + ": ");
+                result = scanner.nextFloat();
+                scanner.nextLine();
+                isFloat = true;
+
+            }catch(Exception e){
+                scanner.nextLine();
+                System.out.println("please enter a number with decimal");
+            }
+        }while (!isFloat);
+
+        return result;
+
+    }
+
+    public static int promptForInt(String prompt) {
+
+        boolean Num = false;
+        int result = 0;
+        do{
+            try{
+                System.out.print(prompt + ": ");
+                result = scanner.nextInt();
+                scanner.nextLine();
+                Num = true;
+            }
+            catch(Exception ex){
+                scanner.nextLine();
+                System.out.println("Invalid Entry, please enter a whole number");
+            }
+        } while (!Num);
+
+        return result;
+    }
+
+    public static long promptForLong(String prompt){
+        System.out.print(prompt + ": ");
+        long result = scanner.nextLong();
+        scanner.nextLine();
+        return result;
+    }
+
+
+}
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,7 +3,55 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
 
-        
+
+        //HOME SCREEN!
+        String mainMenu = """
+         -----LEDGER.APP------
+         
+         Welcome to the Ledger app!
+         -Please choose and option-
+          D- Add Deposit
+          P- Make Payment
+          L- Ledger
+          X- Exit
+          ----------------------
+         """;
+
+        System.out.println(mainMenu);
+
+        char command;
+        command = InputCollector.promptForChar("Enter a command");
+
+        switch(command){
+            case 'D':
+                addDeposit();
+                break;
+            case 'P':
+                addDebitInfo();
+                break;
+            case 'L':
+                ledgerMenu();
+                break;
+            case'X':
+                return;
+            default:
+                System.out.println("Invalid Input! Please choose valid command.");
+                break;
+        }
+
+
 
     }
+
+    //MAIN MENU OPTIONS
+    private static void addDeposit() {
+    }
+
+    private static void addDebitInfo() {
+    }
+
+    private static void ledgerMenu() {
+    }
+
+
 }
